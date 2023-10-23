299723b29491ea146bc2aaab08d3b30fa20d3a4c
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-10-23 10:28:56 +0500

    simple table

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
-import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/react';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -15,6 +18,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [nextui()],
+};
+export default config;
